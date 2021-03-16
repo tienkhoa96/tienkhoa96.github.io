0c6f9bb9ee5bca52a8a0d8fed2134eee6edc45d4
Tien Khoa <tienkhoa280596@GMAIL.COM>
2021-03-16 10:50:11 +0700

    ok
    ok

--- zeplin/main.js
+++ zeplin/main.js
@@ -4,4 +4,4 @@ let input_text = document.getElementsByClassName('input-text')[0]
 input.addEventListener('click', function(){
     input_text.style.display = 'block'
     input_text.focus()
-})
+}) 
