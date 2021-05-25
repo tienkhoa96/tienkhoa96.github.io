c382d7eaa352b77844d33185e428205306bedb3f
Tien Khoa <tienkhoa280596@GMAIL.COM>
2021-05-25 20:16:55 +0700

    ok
    ok

--- Figma/main.js
+++ Figma/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
 
-    // var owl = $('.owl-carousel');
-    // owl.owlCarousel({
-    //     items:3,
-    //     loop:true,
-    //     margin:10,
-    //     autoplay:true,
-    //     // autoplayTimeout:1000,
-    //     // autoplayHoverPause:true
-    // });
-
 
     var story_img_height = $(".story-image img").height();
         $(".story-left").height(story_img_height);
@@ -18,6 +8,61 @@ $(document).ready(function () {
         var story_img_height = $(".story-image img").height();
         $(".story-left").height(story_img_height);
     });
+
+
+    /* buoc 1 get data  tu json sever o day*/
+
+    $.get( "http://localhost:3000/album", function( data ) {
+        //lay data xong thi xem co thanh cong ko
+        // khong thanh cong thi dung code lai
+
+        if(data && data.length>0){
+            // neu thanh cong thi update data vao DOM
+            let album = $(".album-js")
+
+            data.forEach(item => {
+                album.append(
+                    `   
+                        <div class="col-md-4 album-item">
+                            <div class="album-img">
+                                <img src="${item.img}" alt="">
+                            </div>
+                            <div class="album-title">${item.title}</div>
+                        </div>
+                    `
+                )
+            });
+
+        }
+      });
+
+
+      $.get( "http://localhost:3000/dress", function( data ) {
+        //lay data xong thi xem co thanh cong ko
+        // khong thanh cong thi dung code lai
+
+        if(data && data.length>0){
+            // neu thanh cong thi update data vao DOM
+            let album = $(".dress-js")
+
+            data.forEach(item => {
+                album.append(
+                    `
+                        <div class="col-md-3 dress-item">
+                            <img src="${item.img}" alt="">
+                            <div class="dress-item-title">
+                                <p>${item.title}</p>
+                                <div class="dress-item-content">
+                                    ${item.content}
+                                </div>
+                            </div>
+                        </div>
+                    `
+                )
+            });
+
+        }
+      });
 
 
 
