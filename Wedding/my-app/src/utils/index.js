1f841272d1cf7d32169cfd26858dc14278402484
Tien Khoa <tienkhoa280596@GMAIL.COM>
2021-07-16 23:10:28 +0700

    ok
    ok

--- Wedding/my-app/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const saveToLocal = (name, data) => {
-    localStorage.setItem(name, JSON.stringify(data))
-}
-export const getFromLocal = (name) => {
-    let data = localStorage.getItem(name) ? JSON.parse(localStorage.getItem(name)) : {}
-    return data
-}
